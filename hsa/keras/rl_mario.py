e51a9c20f7222618ffb90aa4970f9b41596eff7e
Ferdinand Mathia <FerdinandPeer.Mathia@HS-Augsburg.DE>
2016-08-15 22:10:05 +0200

    now with memory injection

--- hsa/keras/rl_mario.py
+++ hsa/keras/rl_mario.py
@@ -5,9 +5,10 @@ from keras.models import Sequential, model_from_json
 from keras.layers import Dense, Flatten, Input, BatchNormalization
 from keras.optimizers import sgd
 from keras.regularizers import l1
-
+from hsa.keras.qlearning4k import ExperienceReplay
 from hsa import emu_connect
 from hsa.keras.mario_game import MarioEmuGame
+from hsa.keras.memories import load_memories
 from hsa.keras.qlearning4k import Agent
 
 nb_frames = 1
@@ -15,24 +16,28 @@ ram_size = 2048
 nr_actions = 36
 play_period = 20
 batch_size = play_period * 32
-nr_epoch = 15
+nr_epoch = 30
 
 # model_filename = None
-# model_filename = "./1_15_1"
-model_filename = "../dqn_weights/keras/P3_1Layer"
+model_filename = "./M1"
+# model_filename = "../dqn_weights/keras/P3_1Layer"
 
 # Model
 model = Sequential()
 model.add(Flatten(input_shape=(nb_frames, ram_size)))
-model.add(Dense(512, init="glorot_uniform", activation='relu'))
+model.add(Dense(1024, init="glorot_uniform", activation='relu', W_regularizer=l1()))
+model.add(Dense(128, init="glorot_uniform", activation='relu'))
 model.add(Dense(nr_actions))
 model.compile(sgd(lr=10 ** -6, momentum=0.9), "mse")
 
+memory = ExperienceReplay(memory_size=50000)
 if model_filename:
     model.load_weights(model_filename + ".hdf5")
     epsilon = (0.5, 0.05)
 else:
     epsilon = (1.0, 0.1)
+    load_memories(memory)
+
 prim_soc = socket.create_connection(("localhost", 9090))
 prim_soc.setblocking(True)
 emu = emu_connect.Emu2(prim_soc)
@@ -40,7 +45,7 @@ emu.speed_mode("turbo")
 emu.load_slot(10)
 
 game = MarioEmuGame(emu, nr_actions)
-agent = Agent(model, nb_frames=nb_frames)
+agent = Agent(model, memory=memory, nb_frames=nb_frames)
 try:
     agent.train(game, nb_epoch=nr_epoch, epsilon=epsilon, play_period=play_period, batch_size=batch_size,
                 action_repeat=4)
--- hsa/keras/train_from_example.py
+++ hsa/keras/train_from_example.py
@@ -8,35 +8,23 @@ from keras.optimizers import sgd
 from keras.regularizers import l1
 import matplotlib.pyplot as plt
 
-from hsa.dqn_mario.dqn_input import numpy_to_rdqn, rdqn_to_py
+from hsa.keras.memories import load_memories
 from hsa.keras.qlearning4k import ExperienceReplay
-from hsa.reward_evaluation import mario_x_speed
 
-inputs = pandas.read_hdf("../mario_1_1_first.hdf", key="inputs")
-rams = pandas.read_hdf("../mario_1_1_first.hdf", key="rams")
-dqn_inputs = np.array([numpy_to_rdqn(row) for row in inputs.values])
-
-nr_frames = rams.shape[0]
 nr_actions = 36
 # This does not work in this file right now
 nb_frames = 1
 ram_size = 2048
 nr_trainings = 200
 batch_size = 64
-
-memory = ExperienceReplay(nr_frames, fast=False)
+memory = ExperienceReplay(4000, fast=False)
+load_memories(memory)
 
 model = Sequential()
 model.add(Flatten(input_shape=(nb_frames, ram_size)))
 model.add(Dense(512, init="glorot_uniform", activation='relu'))
 model.add(Dense(nr_actions))
 model.compile(sgd(lr=10 ** -6, momentum=0.9), "mse")
-
-
-for i, s_prime in enumerate(rams.values[1:]):
-    s_bloated = np.expand_dims(np.expand_dims(rams.values[i - 1], 0), 0)
-    s_prime_bloated = np.expand_dims(np.expand_dims(s_prime, 0), 0)
-    memory.remember(s_bloated, dqn_inputs[i], mario_x_speed(s_prime), s_prime_bloated, False)
 
 batch = memory.get_batch(model=model, batch_size=batch_size)
 training_loss = list()
