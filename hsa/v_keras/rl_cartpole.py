651e1cf892d9e1e204ddaf1788afb3557e8eb23f
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-10-11 14:49:15 +0200

    actually implemented breakout

--- hsa/v_keras/rl_cartpole.py
+++ hsa/v_keras/rl_cartpole.py
@@ -12,8 +12,7 @@ from hsa.v_keras.qlearning4k.games.game import Game, PlayResult
 
 import gym
 
-#TODO IDEA unify these launch scripts
-env = gym.make('Breakout-ram-v0')
+env = gym.make('CartPole-v0')
 input_size = env.observation_space.shape[0]
 output_size = env.action_space.n
 
@@ -22,15 +21,15 @@ ram_size = input_size
 nr_actions = output_size
 play_period = 5
 batch_size = play_period * 32
-nr_epoch = 1500
-save_every_n_epochs = 25
+nr_epoch = 500
+save_every_n_epochs = 50
 
 model_filename = None
 # memories_filename = "../mario_1_1_third.hdf"
 
 model_name = str(uuid.uuid1())
 # Model hyper parameters
-model = zoo.make_3_hidden_stable(nb_frames, ram_size, nr_actions)
+model = zoo.make_2_hidden_wide_beginning_stable(nb_frames, ram_size, nr_actions)
 
 # second*minute*hour*n
 memory = ExperienceReplay(memory_size=60 * 60 * 60 * 2)
@@ -40,8 +39,8 @@ if model_filename:
     epsilon = (0.1, 0.01)
     epsilon_rate = 0.4
 else:
-    epsilon = (0.8, 0.1)
-    epsilon_rate = 0.3
+    epsilon = (0.8, 0)
+    epsilon_rate = 0.4
 
 
 class BreakoutGame(Game):
@@ -50,7 +49,7 @@ class BreakoutGame(Game):
         self.latest = np.zeros(env.observation_space.shape)
 
     def play(self, action, nr_repeat_actions) -> PlayResult:
-        # env.render()
+        env.render()
         observation, reward, done, info = env.step(action)
         return PlayResult(observation, reward, action, done, False)
 
@@ -60,8 +59,6 @@ class BreakoutGame(Game):
     def get_current_state(self):
         return np.array(self.latest)
 
-print("input: ", input_size)
-print("output: ", output_size)
 
 game = BreakoutGame()
 game.reset()
@@ -74,11 +71,11 @@ try:
     for epoch, epoch_result in enumerate(training_generator):
         print(epoch_result)
         epoch_results.append(epoch_result)
-        if epoch % save_every_n_epochs == 0:
-            model.save_weights("../dqn_weights/keras/tmp/{}_{}.hdf5".format(model_name, epoch))
+        # if epoch % save_every_n_epochs == 0:
+        #     model.save_weights("../dqn_weights/keras/tmp/{}_{}.hdf5".format(model_name, epoch))
 finally:
     print("stopping")
-    model.save_weights("../dqn_weights/keras/tmp/{}_{}.hdf5".format(model_name, "final"))
+    # model.save_weights("../dqn_weights/keras/tmp/{}_{}.hdf5".format(model_name, "final"))
     # noinspection PyTypeChecker
-    pandas.DataFrame.from_dict(epoch_results).to_csv("../dqn_weights/keras/tmp/breakout_{}.csv".format(model_name))
+    pandas.DataFrame.from_dict(epoch_results).to_csv("../dqn_weights/keras/tmp/cartpole_{}.csv".format(model_name))
 
