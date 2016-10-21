adaf5916382c6a8846de1151e306e080877e1b66
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-10-21 15:29:57 +0200

    now with video export

--- hsa/lees_important/fceux_learning_env.py
+++ hsa/lees_important/fceux_learning_env.py
@@ -1,9 +1,11 @@
 from extern.fceux_learningenv.nes_python_interface.nes_python_interface import NESInterface
 import numpy as np
 import matplotlib.pyplot as plt
+import imageio
 
 mario_rom_path = "/home/peer/playground/marionn/Super Mario Bros. (JU) [!].nes"
 nes = NESInterface(mario_rom_path)
+movie_writer = imageio.get_writer("~/mario_video.mp4", fps=60)
 
 nes.reset_game()
 for i in range(10000):
@@ -13,6 +15,7 @@ for i in range(10000):
     else:
         print("run")
         nes.act(4)
+    movie_writer.append_data(nes.getScreenRGB())
     if nes.game_over():
         print("game over")
         nes.reset_game()
@@ -22,3 +25,4 @@ for i in range(10000):
 ram = nes.getRAM()
 
 print(ram)
+movie_writer.close()
