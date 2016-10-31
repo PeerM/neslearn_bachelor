0d7c96190e45a0e9ea06f558cd16981bd78e7e7c
Ferdinand Peer Mathia <ferdinandpeer.mathia@hs-augsburg.de>
2016-10-31 16:55:34 +0100

    optimised launch script

--- hsa/gen3/mario_kerlym.py
+++ hsa/gen3/mario_kerlym.py
@@ -5,23 +5,28 @@ from kerlym import agents
 from kerlym.agents.dqn.networks import simple_dnn
 
 from optparse import OptionParser
+
 parser = OptionParser()
-parser.add_option("-e", "--env", dest="env", default="Mario-ram-v3",                  help="Which GYM Environment to run [%default]")
-parser.add_option("-b", "--batch_size", dest="bs", default=120, type='int',              help="Batch size durring NN training [%default]")
-parser.add_option("-o", "--dropout", dest="dropout", default=0.5, type='float',         help="Dropout rate in Q-Fn NN [%default]")
-parser.add_option("-p", "--epsilon", dest="epsilon", default=0.2, type='float',         help="Exploration(1.0) vs Exploitation(0.0) action probability [%default]")
-parser.add_option("-D", "--epsilon_decay", dest="epsilon_decay", default=1e-6, type='float',    help="Rate of epsilon decay: epsilon*=(1-decay) [%default]")
-parser.add_option("-s", "--epsilon_min", dest="epsilon_min", default=0.05, type='float',help="Min epsilon value after decay [%default]")
-parser.add_option("-d", "--discount", dest="discount", default=0.99, type='float',      help="Discount rate for future reards [%default]")
-parser.add_option("-t", "--num_frames", dest="nframes", default=1, type='int',          help="Number of Sequential observations/timesteps to store in a single example [%default]")
-parser.add_option("-m", "--max_mem", dest="maxmem", default=100000, type='int',         help="Max number of samples to remember [%default]")
-parser.add_option("-P", "--plots", dest="plots", action="store_true", default=False,    help="Plot learning statistics while running [%default]")
-parser.add_option("-F", "--plot_rate", dest="plot_rate", default=1, type='int',        help="Plot update rate in episodes [%default]")
-parser.add_option("-a", "--agent", dest="agent", default="dqn",                         help="Which learning algorithm to use [%default]")
-parser.add_option("-i", "--difference", dest="difference_obs", action="store_true", default=False,  help="Compute Difference Image for Training [%default]")
-parser.add_option("-r", "--learning_rate", dest="learning_rate", type='float', default=1e-4,  help="Learning Rate [%default]")
-parser.add_option("-R", "--render", dest="render", action='store_true', default=False,  help="Render game progress [%default]")
-parser.add_option("-c", "--concurrency", dest="nthreads", type='int', default=1,  help="Number of Worker Threads [%default]")
+parser.add_option("-e", "--env", dest="env", default="Mario-ram-v3", help="Which GYM Environment to run [%default]")
+parser.add_option("-b", "--batch_size", dest="bs", default=120, type='int', help="Batch size durring NN training [%default]")
+parser.add_option("-o", "--dropout", dest="dropout", default=0.5, type='float', help="Dropout rate in Q-Fn NN [%default]")
+parser.add_option("-p", "--epsilon", dest="epsilon", default=0.2, type='float', help="Exploration(1.0) vs Exploitation(0.0) action probability [%default]")
+parser.add_option("-D", "--epsilon_decay", dest="epsilon_decay", default=1e-6, type='float', help="Rate of epsilon decay: epsilon*=(1-decay) [%default]")
+parser.add_option("-s", "--epsilon_min", dest="epsilon_min", default=0.05, type='float', help="Min epsilon value after decay [%default]")
+parser.add_option("-d", "--discount", dest="discount", default=0.99, type='float', help="Discount rate for future reards [%default]")
+parser.add_option("-t", "--num_frames", dest="nframes", default=1, type='int',
+                  help="Number of Sequential observations/timesteps to store in a single example [%default]")
+parser.add_option("-m", "--max_mem", dest="maxmem", default=100000, type='int', help="Max number of samples to remember [%default]")
+parser.add_option("-P", "--plots", dest="plots", action="store_true", default=False, help="Plot learning statistics while running [%default]")
+parser.add_option("-F", "--plot_rate", dest="plot_rate", default=1, type='int', help="Plot update rate in episodes [%default]")
+parser.add_option("-a", "--agent", dest="agent", default="dqn", help="Which learning algorithm to use [%default]")
+parser.add_option("-i", "--difference", dest="difference_obs", action="store_true", default=False, help="Compute Difference Image for Training [%default]")
+parser.add_option("-r", "--learning_rate", dest="learning_rate", type='float', default=1e-4, help="Learning Rate [%default]")
+parser.add_option("-R", "--render", dest="render", action='store_true', default=False, help="Render game progress [%default]")
+parser.add_option("-E", "--render_rate", dest="render_rate", default=2.0, help="[%default]")
+parser.add_option("-n", "--render_ngames", dest="render_ngames", default=2, help="[%default]")
+parser.add_option("-C", "--checkpoint_interval", dest="checkpoint_interval", default=10000, help="[%default]")
+parser.add_option("-c", "--concurrency", dest="nthreads", type='int', default=1, help="Number of Worker Threads [%default]")
 (options, args) = parser.parse_args()
 
 if options.nthreads > 1:
@@ -29,28 +34,26 @@ if options.nthreads > 1:
 else:
     env_factory = lambda: NesEnv(mario_rom_location, frameskip=4)
 
-
-
 agent = agents.DQN(
-                    experiment=options.env,
-                    env=env_factory,
-                    nthreads=options.nthreads,
-                    nframes=1,
-                    epsilon=0.5,
-                    discount=options.discount,
-                    modelfactory=simple_dnn,
-                    epsilon_schedule=lambda episode,epsilon: epsilon*(1-options.epsilon_decay),
-                    batch_size=options.bs,
-                    dropout=options.dropout,
-                    stats_rate=options.plot_rate,
-                    enable_plots = options.plots,
-                    max_memory = options.maxmem,
-                    difference_obs = options.difference_obs,
-                    learning_rate=options.learning_rate,
-                    preprocessor = None,
-                    render = options.render
-                    )
-agent.render_rate_hz = 2.0
-agent.render_ngames = 3
-agent.checkpoint_interval = 10000
+    experiment=options.env,
+    env=env_factory,
+    nthreads=options.nthreads,
+    nframes=1,
+    epsilon=0.5,
+    discount=options.discount,
+    modelfactory=simple_dnn,
+    epsilon_schedule=lambda episode, epsilon: epsilon * (1 - options.epsilon_decay),
+    batch_size=options.bs,
+    dropout=options.dropout,
+    stats_rate=options.plot_rate,
+    enable_plots=options.plots,
+    max_memory=options.maxmem,
+    difference_obs=options.difference_obs,
+    learning_rate=options.learning_rate,
+    preprocessor=None,
+    render=options.render
+)
+agent.render_rate_hz = options.render_rate
+agent.render_ngames = options.render_ngames
+agent.checkpoint_interval = options.checkpoint_interval
 agent.train()
